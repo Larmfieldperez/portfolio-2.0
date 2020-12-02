a3cef08038a4292af7fde13addccc7b805aeecf9
Laura Armfield-Perez <lca13@pitt.edu>
2020-12-02 17:18:10 -0500

    initiate react app plus header

---
+++ src/AboutPopUp.js
@@ -0,0 +1,10 @@
+
+function AboutPopUp() {
+  return (
+    <div className="AboutMe">
+      <h3>peepee poo poo time</h3>
+    </div>
+  )
+}
+
+export default AboutPopUp;
--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import {AboutPopUp} from './AboutPopUp';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Projects</h2>
+        <h2>Contact</h2>
+        <h2>Resume</h2>
       </header>
+
+      <div className="main">
+        <div className="Engineer-Bubble">
+          <h2>Software Enginner</h2>
+        </div>
+
+        <h1>Laura Armfield-Perez</h1>
+
+        <div className="About-Button">
+          <h2>About ME</h2>
+        </div>
+
+      </div>
+
     </div>
   );
 }
