ca17fcb0c38d8a47d01a3f768c3efb3cca0f796a
Laura Armfield-Perez <lca13@pitt.edu>
2020-12-14 14:41:32 -0500

    feat: about popup state hook

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-import {AboutPopUp} from './AboutPopUp';
+import AboutPopUp from './AboutPopUp';
+import React, {useState} from 'react';
 
 function App() {
+  const [aboutState, toggleAboutState] = useState(false)
+
   return (
     <div className="App">
       <header className="App-header">
-        <h2>Projects</h2>
-        <h2>Contact</h2>
-        <h2>Resume</h2>
+        <h2>P r o j e c t s</h2>
+        <h2>C o n t a c t</h2>
+        <h2>R e s u m e</h2>
       </header>
 
       <div className="main">
@@ -17,12 +20,14 @@ function App() {
         </div>
 
         <div className="Name">
-          <h1>Laura Armfield-Perez</h1>
+          <h1>Laura <br></br> Armfield-Perez</h1>
         </div>
 
         <div className="About-Button">
           <h2>About ME</h2>
         </div>
+
+        <div className='aboutPopUpContainer'><AboutPopUp/></div>
 
       </div>
 
