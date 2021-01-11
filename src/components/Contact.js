7cbca8435f089d42533442539ec6b6763b254036
Laura Armfield-Perez <lca13@pitt.edu>
2021-01-11 16:11:14 -0500

    style contact section for desktop and mobile

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,14 +3,19 @@ import './Contact.css';
 function Contact() {
   return (
     <div className="contact-container">
-        <h2>CONTACT</h2>
-        <hr />
+      <div className="inner">
 
-        <div className="contact-links">
-          <a href="http://linkedin.com/in/laura-armfield-perez" target="_blank" rel="noreferrer">linkedin</a>
-          <a href="http://github.com/larmfieldperez" target="_blank" rel="noreferrer">github</a>
-          <a href="mailto:l.armfieldperez@gmail.com">email</a>
-        </div>
+          <h2>CONTACT</h2>
+
+          <hr />
+
+          <div className="contact-links">
+            <a href="http://linkedin.com/in/laura-armfield-perez" target="_blank" rel="noreferrer">linkedin</a>
+            <a href="http://github.com/larmfieldperez" target="_blank" rel="noreferrer">github</a>
+            <a href="mailto:l.armfieldperez@gmail.com">email</a>
+          </div>
+
+      </div>
     </div>
   )
 }
