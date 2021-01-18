45c3352fbe3356f748ef77d27728530873aa003c
Laura Armfield-Perez <lca13@pitt.edu>
2021-01-18 17:56:57 -0500

    fade in

--- src/components/AllProjects.js
+++ src/components/AllProjects.js
@@ -3,6 +3,7 @@ import projects from './ProjectsList';
 import SingleProject from './SingleProject';
 
 import LazyLoad from 'react-lazyload';
+import FadeIn from 'react-fade-in';
 
 function AllProjects() {
   return (
@@ -10,8 +11,10 @@ function AllProjects() {
       {projects.map(project => {
         return (
             <div>
-              <LazyLoad offset={-150} throttle={100} >
+              <LazyLoad throttle={200} delay={200}>
+              <FadeIn delay={500} transitionDuration={600}>
               <SingleProject project={project} key={project.name} />
+              </FadeIn>
               </LazyLoad>
             </div>
 
