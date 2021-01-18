ec3150844c5247cd744fbff9991c0a7dd87b641d
Laura Armfield-Perez <lca13@pitt.edu>
2021-01-18 15:20:35 -0500

    header styled for web and mobile

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,18 @@ import './Header.css'
 function Header() {
   return (
     <header className="App-header">
-      <h2>P r o j e c t s</h2>
-      <h2>C o n t a c t</h2>
-      <h2>R e s u m e</h2>
+      <div>
+        <span>Projects</span>
+      </div>
+
+      <div>
+        <span>Contact</span>
+      </div>
+
+      <div>
+        <span>Resume</span>
+      </div>
+
     </header>
   )
 }
