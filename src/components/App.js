d6f6e0eaaae165ba791b189d6d9eae56130d6bf3
Laura Armfield-Perez <lca13@pitt.edu>
2021-01-18 14:32:50 -0500

    move header out into separate component

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ import AboutPopUp from './AboutPopUp';
 import FeaturedProject from './FeaturedProject';
 import AllProjects from './AllProjects';
 import Contact from './Contact';
+import Header from './Header'
 
 
 function App() {
@@ -21,11 +22,7 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <h2>P r o j e c t s</h2>
-        <h2>C o n t a c t</h2>
-        <h2>R e s u m e</h2>
-      </header>
+      <Header />
 
       <div className="main">
 
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import './Header.css'
+
+function Header() {
+  return (
+    <header className="App-header">
+      <h2>P r o j e c t s</h2>
+      <h2>C o n t a c t</h2>
+      <h2>R e s u m e</h2>
+    </header>
+  )
+}
+
+export default Header;
